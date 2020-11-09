c3fd465d00783fc48752414db5d15877fec6c399
Josh Aragon <josharagon99@gmail.com>
2020-11-09 12:06:28 -0700

    Add layout and basic game functionality

---
+++ src/player.js
@@ -0,0 +1,17 @@
+
+class Player{
+  constructor(token) {
+    this.id = Date.now();
+    this.wins = 0;
+  }
+  winnerWinner(){
+    this.wins += 1;
+  }
+  saveWinsToStorage(){
+
+  }
+
+  retrieveWinsFromStorage(){
+
+  }
+}
